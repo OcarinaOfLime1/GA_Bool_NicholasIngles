8efae1f85b27d4513ee56d621f7ee36ed7a00a52
Nicholas Ingles <nringles@student.rtc.edu>
2024-02-10 20:12:03 -0800

    Add project files.

---
+++ GA_Bool_NicholasIngles/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(isAdult);
+        bool isAdult = true;
+
+
+    }
+}
